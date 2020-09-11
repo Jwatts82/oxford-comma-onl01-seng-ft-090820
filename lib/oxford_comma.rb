962271b5978234140bcfa2240abdc43cbe610634
Jessica Watts <jessperr182@hotmail.com>
2020-09-11 05:46:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,11 @@
 def oxford_comma(array)
-  if array.length == 2
-    array.join(" and ")
-    elsif array.length > 2
-      array.insert(-2, "and")
-      array[0..-1].join(", ")
-    elsif array.length > 3
-      array.insert(-3, "and")
-      array[0..1].join(",")
-    else
-      array.join
+
+  if array.length == 1
+end 	    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return "#{array[0..-2].join(", ")}, and #{array.last}"
   end
 end
   
